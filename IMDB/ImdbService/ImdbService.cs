98c04e0ac6a934b9759280304bc06c5d1f06b673
Andreas Dahl Sørensen <anso@itu.dk>
2013-11-26 10:54:16 +0100

    Created the web service layer

---
+++ IMDB/ImdbService/ImdbService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using ASP_Client.Models;
+
+namespace ImdbService {
+	/// <inheritdoc/>
+	public class ImdbService : IImdbService {
+
+		/// <inheritdoc />
+		public List<MovieViewModel> GetMoviesByTitle(string needle)
+		{
+			using (var entities = new ImdbEntities())
+			{
+				if (String.IsNullOrEmpty(needle))
+				{
+					return (from m in entities.Movies
+						select new MovieViewModel() {
+							Id = m.Id,
+							Title = m.Title,
+							Year = m.Year
+						}).ToList();
+				}
+				else
+				{
+					return (from m in entities.Movies where m.Title.Contains(needle)
+					select new MovieViewModel()
+					{
+						Id = m.Id,
+						Title = m.Title,
+						Year = m.Year
+					}).ToList();
+				}
+			}
+		} // GetMoviesByTitle()
+	}
+}
