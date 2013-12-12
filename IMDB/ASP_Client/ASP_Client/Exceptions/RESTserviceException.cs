279c027b66ee4b913b9ef3a206424e00426f9273
Lapras <ASehr@hotmail.de>
2013-12-12 10:51:13 +0100

    Added ASP Client connection exceptions (same as WPF)

---
+++ IMDB/ASP_Client/ASP_Client/Exceptions/RESTserviceException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace WPF_Client.Exceptions
+{
+    /// <summary>
+    /// Exception for when there is an exception in the REST service strategy.
+    /// </summary>
+    class RESTserviceException : Exception
+    {
+        public RESTserviceException()
+            : base() { }
+
+        public RESTserviceException(string message)
+            : base(message) { }
+
+        public RESTserviceException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+        public RESTserviceException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public RESTserviceException(string format, Exception innerException, params object[] args)
+            : base(string.Format(format, args), innerException) { }
+    }
+}
---
+++ IMDB/ASP_Client/ASP_Client/Exceptions/UnavailableConnectionException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Client.Exceptions
+{
+    /// <summary>
+    /// Exception for when there is no available connection.
+    /// </summary>
+    class UnavailableConnectionException : Exception
+    {
+        public UnavailableConnectionException()
+            : base() { }
+
+        public UnavailableConnectionException(string message)
+            : base(message) { }
+
+        public UnavailableConnectionException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+        public UnavailableConnectionException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public UnavailableConnectionException(string format, Exception innerException, params object[] args)
+            : base(string.Format(format, args), innerException) { }
+    }
+}
