3605e0aab0afb175e0a567bb155004452b6fd45c
Andreas Dahl Sørensen <anso@itu.dk>
2013-12-14 21:11:00 +0100

    avg rating is cast to float value

--- IMDB/dbChanges/changes003.sql
+++ IMDB/dbChanges/changes003.sql
@@ -9,7 +9,7 @@ DECLARE @MovieId int, @Avg_rating float
 SET @MovieId = (SELECT movie_id FROM inserted)
 SET @Avg_rating = (
 	SELECT
-		AVG([Rating].[rating]) AS avg_rating
+		CAST(AVG([Rating].[rating]) AS float) AS avg_rating
 	FROM
 		[Rating]
 	WHERE
