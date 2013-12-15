12a66c4d396cd73b1a34fca7c7a4fb47e4f20f90
Lapras <ASehr@hotmail.de>
2013-12-15 11:40:16 +0100

    Fixed session timout at ASP.Net at 60 minutes

--- IMDB/ASP_Client/ASP_Client/Session/UserSession.cs
+++ IMDB/ASP_Client/ASP_Client/Session/UserSession.cs
@@ -28,7 +28,7 @@ namespace ASP_Client.Session
         public void Login(UserModel user)
         {
             _httpSessionStateBase.Add("User", user);
-            _httpSessionStateBase.Timeout = 1;
+            _httpSessionStateBase.Timeout = 60;
         }
 
         /// <summary>
